74d480050bc6d7816c9d3b760eb023f6c0391db5
IgorGultyaev <igor.gultyaev@mail.ru>
2022-05-19 17:43:09 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        //демонстрация
+
+        System.out.println(calc.sum(2, 4));
+        System.out.println(calc.mult(2, 4));
+        System.out.println(calc.pow(2, 4));
+
+    }
+}
